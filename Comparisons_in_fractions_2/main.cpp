a73d5b4eaa4d520828c0c10100106b1e4d498248
Alexander Nesterov <alex18nest@icloud.com>
2024-09-02 16:48:54 -0300

    Initial Commit

---
+++ Comparisons_in_fractions_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Comparisons_in_fractions_2
+//
+//  Created by Александр Нестеров on 2/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
